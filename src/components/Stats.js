fd19b6aebace4702c58f76060f5d820f5f66f89c
juanmnl <juanmnlcornejo@gmail.com>
2020-03-19 00:21:07 -0500

    Fix number formating on world data

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -3,6 +3,7 @@ import useStats from '../hooks/useStats';
 import { StatGrid, StatBlock } from '../components/StyledStats';
 
 export default function Stats({ url }) {
+  const nf = Intl.NumberFormat();
   const { stats, loading, error } = useStats(url);
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Something went wrong</p>;
@@ -15,15 +16,15 @@ export default function Stats({ url }) {
       </p>
       <StatGrid>
         <StatBlock>
-          <p>{stats.confirmed.value}</p>
+          <p>{nf.format(stats.confirmed.value)}</p>
           <h3>Confirmados</h3>
         </StatBlock>
         <StatBlock>
-          <p>{stats.deaths.value}</p>
+          <p>{nf.format(stats.deaths.value)}</p>
           <h3>Decesos</h3>
         </StatBlock>
         <StatBlock>
-          <p>{stats.recovered.value}</p>
+          <p>{nf.format(stats.recovered.value)}</p>
           <h3>Recuperados</h3>
         </StatBlock>
       </StatGrid>
