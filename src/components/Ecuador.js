57c45c32d789c54cd4e26c9bcf9a1d3292d6345a
juanmnl <juanmnlcornejo@gmail.com>
2020-03-27 17:51:43 -0500

    Fix updated time

--- src/components/Ecuador.js
+++ src/components/Ecuador.js
@@ -33,7 +33,7 @@ const Ecuador = ({ t }) => {
     <>
       <p>
         <small>
-          {t('updateDate.label')} 25.03 | 18:30 | {t('source.label')}{' '}
+          {t('updateDate.label')} 27.03 | 17:30 | {t('source.label')}{' '}
           <a
             target="_blank"
             rel="noopener noreferrer"
